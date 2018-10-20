f0c965c3371c7c4dfb0d7ee2ff0781f8c21ec8cf
micheldanjou <michel.danjou@gmail.com>
2018-10-20 13:00:10 +0100

    Code cleanup

--- Etivity5.py
+++ Etivity5.py
@@ -1,3 +1,4 @@
+
 import numpy as np
 import random as rand
 import matplotlib.pyplot as plt
@@ -8,7 +9,6 @@ from sklearn.decomposition import PCA
 class Log:
     DEBUG = 1
     INFO = 2
-    ALWAYS_SHOW = 3
 
 
 class My_pca:
@@ -18,24 +18,12 @@ class My_pca:
     There is a lot of log statements in this class. I intend to remove
     them in the final code. Leaving them in place for the time being as they
     are useful for debugging. 
-    
-    BUG ALERT
-    =========
-    The eigen values calculated by this class match the ones calculated by 
-    scikit. 
-    
-    However, it appears that one of the eigen vectors is the negative version 
-    of the one calculted by scikit. Currently investigating the reason behind 
-    this.
         
     """
 
     log_level = Log.INFO
     nb_components = 2
-    eigen_values = []
-    eigen_vectors = []
-    c_matrix = []
-
+   
 
     def __init__(self):
         """ init """
@@ -51,74 +39,75 @@ class My_pca:
         
     def fit(self, matrix):
         """ 
-        Explicitly using array manipulation instead of 
-        easier matrix operations 
         """
         self.matrix = matrix
         
         # Calculate mean values of each column from dataset
         m0 = np.mean(self.matrix[:,0])
         m1 = np.mean(self.matrix[:,1])
-        self.__log__("mean.col0:{} ".format(m0))
-        self.__log__("mean.col1:{} ".format(m1))       
         
         # Center the columns by subtracting the corresponding mean
         c0 = matrix[:,0] - m0
         c1 = matrix[:,1] - m1
-        self.__log__("c0       :{} ".format(c0), Log.DEBUG)
-        self.__log__("c1       :{} ".format(c1), Log.DEBUG)       
         
         # Create a centered matrix 
         self.c_matrix = []
         self.c_matrix = np.append(c0, c1, axis=1)
-        self.__log__("centered_matrix:\n{}".format(self.c_matrix), Log.DEBUG)
         
         # Calculate covariance of centered matrix
         my_cov = np.cov(self.c_matrix, rowvar=False)        
-        self.__log__("covariance:\n{}".format(my_cov), Log.DEBUG)
     
         # eigen values, eigen vectors
         eigen_values, eigen_vectors = eig(my_cov)
-        self.__log__("eigen_values:\n{}".format(eigen_values), Log.INFO)
-        self.__log__("eigen_vectors:\n{}".format(eigen_vectors), Log.INFO)     
         
         # order eigen values and eigen vectors       
         sorted_eigen_values_indexes = eigen_values.argsort()[::-1]
         sorted_eigen_values = eigen_values[sorted_eigen_values_indexes]
-        sorted_eigen_vectors = eigen_vectors[sorted_eigen_values_indexes] 
-        self.__log__("sorted_eigen_values_indexes:\n{}".format(sorted_eigen_values_indexes), Log.DEBUG)
-        self.__log__("sorted_eigen_values:\n{}".format(sorted_eigen_values), Log.DEBUG)
-        self.__log__("sorted_eigen_vectors:\n{}".format(sorted_eigen_vectors), Log.DEBUG)
+        sorted_eigen_vectors = eigen_vectors.T[sorted_eigen_values_indexes]
 
         # use nb_components to decide how many eigen vectors to keep
         filtered_sorted_eigen_values = sorted_eigen_values[:self.nb_components]
         filtered_sorted_eigen_vectors = sorted_eigen_vectors[:self.nb_components] 
-        self.__log__("filtered_sorted_eigen_values:\n{}".format(filtered_sorted_eigen_values), Log.INFO)
-        self.__log__("filtered_sorted_eigen_vectors:\n{}".format(filtered_sorted_eigen_vectors), Log.INFO)
         
         # save results as class variables
         self.eigen_values = filtered_sorted_eigen_values
         self.eigen_vectors = filtered_sorted_eigen_vectors
-            
+        
+        return(self.eigen_values, self.eigen_vectors)
+         
     
     def transform(self, data):
         """
         Calculate projection of dataset onto the eigen vector basis
         """
         self.fit(data)
+        self.__log__("="*80)
+        self.__log__("PCA Homebrew nb_components={}\n".format(self.nb_components))
         
         self.__log__("eigen_values shape:{}".format(self.eigen_values.shape), Log.DEBUG)
-        self.__log__("eigen_values:\n{}".format(self.eigen_values), Log.DEBUG)        
+        self.__log__("eigen_values:\n{}".format(self.eigen_values), Log.INFO)        
+
         self.__log__("eigen_vectors shape:\n{}".format(self.eigen_vectors.shape), Log.DEBUG)
-        self.__log__("eigen_vectors:\n{}".format(self.eigen_vectors), Log.DEBUG)
+        self.__log__("eigen_vectors:\n{}".format(self.eigen_vectors), Log.INFO)
+
+        self.__log__("self.c_matrix shape:\n{}".format(self.c_matrix.shape), Log.DEBUG)
+        self.__log__("self.c_matrix:\n{}".format(self.c_matrix), Log.DEBUG)
         
-        self.projection = self.eigen_vectors.T.dot(self.c_matrix.T).T
+        self.projection = self.eigen_vectors.dot(self.c_matrix.T).T
         
-        self.__log__("projected shape :\n{}".format(self.projection.shape), Log.INFO)
+        self.__log__("projected shape :\n{}".format(self.projection.shape), Log.DEBUG)
         self.__log__("projected  :\n{}".format(self.projection), Log.INFO)
         
+        self.draw()
+        return (self.eigen_values, self.eigen_vectors, self.projection)
+        
+
+    def draw(self):
         plt.plot(data[:,0], data[:,1], 'bo')
-        plt.plot(self.projection[:,0], self.projection[:,1], 'xr')
+        if self.nb_components == 2:
+            plt.plot(self.projection[:,0], self.projection[:,1], 'xr')
+        elif self.nb_components == 1:
+            plt.plot(self.projection, np.zeros_like(self.projection),'xr', label="MyPCA Transformed dataset")            
         plt.show()
         
 
@@ -147,15 +136,18 @@ def scikit_pca( matrix, nb_components):
     pca.fit(matrix)
     projection = pca.transform(matrix)
     
-    if nb_components == 2:
-        plt.plot(data[:,0], data[:,1], 'bo')
-        plt.plot(projection[:,0], projection[:,1], 'xr')
-        plt.show()
-
     print("Eigen_values:", pca.explained_variance_)
     print("Eigen_vectors:", pca.components_)
     print("Transformed_data:", pca.transform(data))
         
+    plt.plot(data[:,0], data[:,1], 'bo')
+
+    if nb_components == 2:
+        plt.plot(projection[:,0], projection[:,1], 'xr')
+    elif nb_components == 1:
+        plt.plot(projection, np.zeros_like(projection),'xr', label="MyPCA Transformed dataset")            
+    plt.show()
+     
     return pca
 
       
@@ -170,17 +162,17 @@ def test():
     pca = scikit_pca(data, 1)
     
     # Calculate PCA using homebrew code, nb_components=2
+    
+    # Please note that the user can call the fit() API also. 
+    # We are not doing it here because fit() is called by transform() internally
+    # So this API works my_pca.fit(data) but for brevity's sake I am simply 
+    # calling my_pca.transform(data)
     my_pca.nb_components=2
-    print("="*80)
-    print("PCA Homebrew nb_components=", my_pca.nb_components, "\n")
-    #my_pca.fit(data)
     my_pca.transform(data)
         
     # Calculate PCA using homebrew code, nb_components=1
     my_pca.nb_components=1
-    print("="*80)
-    print("PCA Homebrew nb_components=", my_pca.nb_components, "\n")
-    my_pca.fit(data)
+    my_pca.transform(data)
     
 
 data = build_dataset()    
